32f2f7e0ae854f07e6e37be0a80130ac4b27a88b
robert-angelo19 <robert.angelo19@unifesp.br>
2025-11-08 11:23:26 -0300

    correcao de bug quando dono do projeto editava ou gerenciava membros ele saía do projeto

--- core/forms.py
+++ core/forms.py
@@ -20,10 +20,21 @@ class ProjetoForm(forms.ModelForm):
         super(ProjetoForm, self).__init__(*args, **kwargs)
         
         if user:
-            # Filtra empresas: apenas as que o usuário pode acessar
+            # filtra empresas: apenas as que o usuario pode acessar
             self.fields['empresa'].queryset = Empresa.objects.filter(
                 Q(criador=user) | Q(projetos__membros=user)
             ).distinct()
             
-            # Filtra membros: exclui o próprio usuário da lista de membros
+            
+        if self.instance and self.instance.pk:
+            #editando projeto existente mantem o usuario atual se ja for membro
+            current_members = self.instance.membros.all()
+            if user in current_members:
+                #inclui o usuario na lista para manter a selecao
+                self.fields['membros'].queryset = self.fields['membros'].queryset
+            else:
+                # exclui o usuario da lista
+                self.fields['membros'].queryset = self.fields['membros'].queryset.exclude(id=user.id)
+        else:
+            # criando novo projeto exclui o usuario da lista
             self.fields['membros'].queryset = self.fields['membros'].queryset.exclude(id=user.id)
